a68c2cc89b077625598bb141e24ac1914b792859
Duncan Bellamy <dunk@denkimushi.com>
2020-09-13 10:21:19 +0100

    add sync to migrate.sh update readme

--- migrate.sh
+++ migrate.sh
@@ -3,6 +3,8 @@ allusers="user1@ex.com user2@ex.com user3@ex.com user4@ex.com"
 sshcmd="ssh doveback@127.0.0.1 -p 2222 -o \"UserKnownHostsFile /dev/null\""
 echo "Syncing Mail..."
 for usname in ${allusers}; do
-  echo "Syncing $usname"
+  echo "Backing up $usname"
   sudo doveadm backup -u $usname $sshcmd doas doveadm dsync-server -u $usname
+#  echo "Syncing up $usname"
+#  sudo doveadm sync -u $usname $sshcmd doas doveadm dsync-server -u $usname
 done
